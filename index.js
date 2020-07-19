e00da1484e234c9defacab3c1cc498875f218370
bertac_severcan <bertac.severcan@icloud.com>
2020-07-19 12:46:51 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution here!
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
+const pet = "Odie";
+append.push(pet);
+prepend.unshift(pet);
+removeLast.pop();
+removeFirst.shift();
